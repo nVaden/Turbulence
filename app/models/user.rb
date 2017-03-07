b925f1c5bb457baa9d5dff2306a92fa1ad9b8477
Nicholas Vaden <nickvaden11@Live.com>
2017-03-07 13:59:40 -0500

    Paperclip, Devise, Table creations(Users,Posts,Comments)

---
+++ app/models/user.rb
@@ -0,0 +1,10 @@
+class User < ActiveRecord::Base
+  has_many :posts
+  has_many :comments, :through => :posts
+  has_attached_file :image, styles: { :medium => "640x" }
+  validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Turbulence::Application.routes.draw do
+  resources :comments
+
+  resources :posts
+
+  devise_for :users
+    root to: "posts#index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
