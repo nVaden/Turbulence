94247c32a9ffba3628b65cd43e9b14cea469a462
Nicholas Vaden <nickvaden11@Live.com>
2017-03-07 12:33:29 -0500

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Turbulence::Application.config.secret_key_base = 'b0657c6622f45e74d9e92f04f6573e4f55961bd495bac7280a3f4583e0c390d875bbd059994ac465b761b355940b7a50ac66c8bb543fbfc742dfcd2f3ea43507'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Turbulence::Application.config.session_store :cookie_store, key: '_Turbulence_session'
